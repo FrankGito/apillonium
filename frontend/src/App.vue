0c18ef44bc51cd57994a87669a6430f855cadf99
Frank Dierolf <frank_dierolf@web.de>
2024-06-02 23:48:20 +0200

    :zap: (frontend) intialise 3D Scene with TresJS

---
+++ frontend/src/App.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import Experience from './components/Experience.vue'
+</script>
+
+<template>
+  <Suspense>
+    <Experience />
+  </Suspense>
+</template>
+
+<style>
+html,
+body {
+  margin: 0;
+  padding: 0;
+  height: 100%;
+  width: 100%;
+}
+
+#app {
+  height: 100%;
+  width: 100%;
+}
+</style>
---
+++ frontend/src/components/model.vue
@@ -0,0 +1,12 @@
+<script setup>
+import { useGLTF } from '@tresjs/cientos';
+
+const { scene: model } = await useGLTF(
+  'https://raw.githubusercontent.com/Tresjs/assets/main/models/gltf/blender-cube.glb',
+  { draco: true }
+);
+</script>
+
+<template>
+  <primitive :object="model" />
+</template>
