3708655e4a677c66f42b9fbe58d438a655c138d5
Frank Dierolf <frank_dierolf@web.de>
2024-06-03 16:26:57 -0500

    :zap: (nifty) add createCollection.ts

---
+++ nifty/createCollection.ts
@@ -0,0 +1,28 @@
+import {
+  CollectionType,
+  SubstrateChain,
+  LogLevel,
+  Nft,
+} from '@apillon/sdk';
+
+const nft = new Nft({
+  key: 'a51d1f95-c56a-49c4-ac54-08abf3567ab1',
+  secret: 'rfFfWu6wWlpt',
+  logLevel: LogLevel.VERBOSE,
+});
+
+let substrateCollection = await nft.createSubstrate({
+  chain: SubstrateChain.ASTAR,
+  collectionType: CollectionType.GENERIC,
+  name: 'Frankos',
+  symbol: 'FO',
+  baseUri: 'https://moonbeamnfts.com/collections/spaceexplorers/',
+  description: 'Frankos are a uprising NFT Collection',
+  baseExtension: 'json',
+  royaltiesFees: 5,
+  drop: false,
+  maxSupply: 100,
+});
+
+const nftCollection = await substrateCollection.get()
+console.log(nftCollection.uuid)
