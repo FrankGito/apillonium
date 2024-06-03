9d8e428e80b6df741cc5ccce1078d26c8211f8af
Frank Dierolf <frank_dierolf@web.de>
2024-06-03 18:21:51 -0500

    :zap: (nifty) burn only works if revokable was true

--- nifty/burnOnCollection.ts
+++ nifty/burnOnCollection.ts
@@ -1,5 +1,8 @@
 import { getCollection } from "./getCollection";
 
+/*--------------------*/
+/*--HAS TO REVOKABLE--*/
+/*--------------------*/
 const substrateCollection = await getCollection()
 const burn = async (tokenId: string) => await substrateCollection.burn(tokenId);
 
