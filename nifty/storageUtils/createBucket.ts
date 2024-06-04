4c3b5cecfb7920761eb55ced1a6960fe14bc8c4e
Frank Dierolf <frank_dierolf@web.de>
2024-06-04 09:20:38 -0500

    :zap: (nifty) modify creatBucket.ts

--- nifty/storageUtils/createBucket.ts
+++ nifty/storageUtils/createBucket.ts
@@ -7,11 +7,8 @@ const storage = new Storage({
   logLevel: LogLevel.VERBOSE,
 });
 
-// list buckets
-await storage.listBuckets({ limit: 5 });
-
-// create and instance of a bucket directly through uuid
-const bucket = storage.bucket("uuid");
+// Bucket has to be created on the UI 
+const bucket = storage.bucket("c3952273-74b4-4222-8fd6-70fea842ede3");
 
 const jsonBuffer = fs.readFileSync("./1.json");
 const result = await bucket.uploadFiles([
@@ -21,28 +18,5 @@ const result = await bucket.uploadFiles([
     content: jsonBuffer,
   },
 ]);
+
 console.log(result);
-
-/**
-// list objects (files, folders) in a bucket
-await bucket.listObjects({
-  directoryUuid: 'eaff2672-3012-46fb-9278-5efacc6cb616',
-  markedForDeletion: false,
-  limit: 5,
-});
-
-// list all files in a bucket no matter if they are in a folder or not
-await bucket.listFiles({ fileStatus: FileStatus.UPLOADED });
-
-// generate an IPFS link for a CID
-// const cid = 'bafybeigjhyc2tpvqfqsuvf3byo4e4a4v6spi6jk4qqvvtlpca6rsaf2cqi';
-// const link = await storage.generateIpfsLink(cid);
-
-// gets a specific file in a bucket directly through uuid
-const file = await bucket.file('2195521d-15cc-4f6e-abf2-13866f9c6e03').get();
-
-// deletes a file via uuid
-await bucket.file('2195521d-15cc-4f6e-abf2-13866f9c6e03').delete();
-// deletes a directory via uuid
-await bucket.directory('eddc52cf-92d2-436e-b6de-42d7cad621c3').delete();
-*/
