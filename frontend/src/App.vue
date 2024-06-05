30182025693649bf534641ada9b52405b2069759
Frank Dierolf <frank_dierolf@web.de>
2024-06-05 07:24:39 -0500

    :zap: (frontend) change scrollbar style

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,23 +1,26 @@
 <template>
   <main class="min-h-screen flex flex-col font-unbounded">
-    <nav
-      class="flex flex-row gap-8 justify-center font-bold text-xl space-x-4 p-6"
-    >
-      <router-link class="text-blue-500 hover:text-blue-800" to="/"
-        >Home</router-link
-      >
-      <router-link class="text-blue-500 hover:text-blue-800" to="/experience"
-        >Experience</router-link
-      >
-      <router-link class="text-blue-500 hover:text-blue-800" to="/about"
-        >About</router-link
-      >
-      <router-link class="text-blue-500 hover:text-blue-800" to="/team"
-        >Team</router-link
-      >
+    <nav class="flex flex-row gap-8 justify-center font-bold text-xl space-x-4 p-6">
+      <router-link class="text-blue-500 hover:text-blue-800" to="/">Home</router-link>
+      <router-link class="text-blue-500 hover:text-blue-800" to="/experience">Experience</router-link>
+      <router-link class="text-blue-500 hover:text-blue-800" to="/about">About</router-link>
+      <router-link class="text-blue-500 hover:text-blue-800" to="/team">Team</router-link>
     </nav>
     <div class="flex flex-col w-full items-center justify-center">
       <router-view></router-view>
     </div>
   </main>
 </template>
+<style>
+::-webkit-scrollbar {
+  width: 3px;
+}
+
+::-webkit-scrollbar-track {
+  background: white;
+}
+
+::-webkit-scrollbar-thumb {
+  background: black;
+}
+</style>
