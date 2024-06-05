4e60b35a051908bd107ca691ff2b84152b48bdcb
Opeyemi Oginni <107570612+OpeOginni@users.noreply.github.com>
2024-06-05 12:07:10 +0100

    Fixed Spelling mistake

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -10,7 +10,7 @@ function goToExperience() {
 
 <template>
   <div class="flex flex-col gap-5 justify-center items-center min-h-screen">
-    <h1 class="text-4xl">Welcome To Appilion</h1>
+    <h1 class="text-4xl">Welcome To Apillonium</h1>
     <Button @click="goToExperience" class="ml- mt-7">Go to Experience</Button>
   </div>
 </template>
